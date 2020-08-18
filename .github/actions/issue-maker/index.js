08412e98e1e013d0ce055f2101bc242b3fe1d7c5
Thomas <thsbrown1@gmail.com>
2020-08-17 17:15:33 -0700

    remove try catch

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -3,20 +3,15 @@ const github = require("@actions/github");
 
 async function run() {
 
-    try {
-        const issueTitle = core.getInput("issue-title");
-        const repoToken = core.getInput("repo-token");
-        const joke = core.getInput("joke");
-        const octokit = github.getOctokit(repoToken);
+    const issueTitle = core.getInput("issue-title");
+    const repoToken = core.getInput("repo-token");
+    const joke = core.getInput("joke");
+    const octokit = github.getOctokit(repoToken);
 
-        const newIssue = await octokit.issues.create({
-            title: issueTitle,
-            body:joke
-        });
-    }catch(exception){
-        core.setOutput(exception);
-        core.setFailed("Failed to create issue");
-    }
+    const newIssue = await octokit.issues.create({
+        title: issueTitle,
+        body:joke
+    });
 }
 
 run();
